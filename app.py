066552431c8f1f432ab262f43ab16dfc9c8d7ace
kutayy <kutay.yegul@humanitec.com>
2020-02-24 16:24:00 +0100

    covering no connection  case

--- app.py
+++ app.py
@@ -6,23 +6,26 @@ app = Flask(__name__)
 
 @app.route('/<parameter>')
 def hello_world(parameter=None):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue=parameter)
-    channel.basic_publish(exchange='',
-                          routing_key=parameter,
-                          body=f'Hello World from {parameter}!')
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue=parameter)
+        channel.basic_publish(exchange='',
+                              routing_key=parameter,
+                              body=f'Hello World from {parameter}!')
 
-    method_frame, header_frame, body = channel.basic_get(parameter)
-    if method_frame:
-        channel.basic_ack(method_frame.delivery_tag)
-        channel.queue_delete(queue=parameter)
-        connection.close()
-        return body
-    else:
-        channel.queue_delete(queue=parameter)
-        connection.close()
-        return 'No message returned'
+        method_frame, header_frame, body = channel.basic_get(parameter)
+        if method_frame:
+            channel.basic_ack(method_frame.delivery_tag)
+            channel.queue_delete(queue=parameter)
+            connection.close()
+            return body
+        else:
+            channel.queue_delete(queue=parameter)
+            connection.close()
+            return 'No message returned'
+    except Exception as e:
+        return f"Possibly no RabbitMQ connection \nException : {e}"
 
 
 if __name__ == '__main__':
